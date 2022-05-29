cf8d7c77686ab856f3b1fec0c36c7ccac0d8fe4c
supreetoh <supreetoh@gmail.com>
2022-05-29 22:11:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrls')
+const app = express()
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true, useUnifiedTopology:true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req,res)=> {
+    res.render('index')
+})
+
+app.post('/shortUrls', async(req,res)=>{
+   await ShortUrl.create({ full: req.body.fullUrl })
+   res.redirect('/')
+})
+
+
+
+app.listen(process.env.PORT || 5000);
